e568c5c286d4cb14b92935a4d5b1b5011d50a480
Rokib <syerokib@bell.corp.bce.ca>
2018-11-03 13:51:53 -0400

    adfs

--- DataParser/src/main/java/com/example/demo/DataParserController.java
+++ DataParser/src/main/java/com/example/demo/DataParserController.java
@@ -1,28 +1,28 @@
 package com.example.demo;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
 
 
 @RestController
 public class DataParserController {
 
-	@RequestMapping(
-    value = "/process", 
-    method = RequestMethod.POST,
-    consumes = "application/json")
-public String process(@RequestBody DeviceData deviceData) throws Exception {
+    @RequestMapping(
+            value = "/process",
+            method = RequestMethod.POST,
+            consumes = "application/json")
 
-  return deviceData.toString();
+    public String process(@RequestBody DeviceData deviceData) throws Exception {
+        return deviceData.toString();
+    }
 
-//        return
-}
+    @RequestMapping(
+            value = "/process",
+            method = RequestMethod.GET)
 
+    public String process(String hello) {
+        return "HELLO WORLD!";
+    }
 }
