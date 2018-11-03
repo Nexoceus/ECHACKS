4d38ce61b12b6484e61edb930971c54b11229d96
Rokib <syerokib@bell.corp.bce.ca>
2018-11-03 05:19:18 -0400

    added post mapping

--- DataParser/src/main/java/com/example/demo/DataParserController.java
+++ DataParser/src/main/java/com/example/demo/DataParserController.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DataParserController {
 
-    @RequestMapping(value = "*", method = RequestMethod.GET)
-    public String hello() {
-        return "hello world!";
-    }
+//    @RequestMapping(value = "/submitdata", method = RequestMethod.POST)
+//    public String dataReceptionResponse() {
+////        return "hello world!";
+//        Req
+//    }
 
+    @PostMapping("/request")
+    public String postController(@RequestBody DeviceData deviceData) {
+        return deviceData.toString();
+    }
 }
---
+++ DataParser/src/main/java/com/example/demo/DeviceData.java
@@ -0,0 +1,86 @@
+package com.example.demo;
+
+public class DeviceData {
+
+    private double o2val;
+    private double co2val;
+    private double vocval;
+    private double temperature;
+    private double humidity;
+    private double latitude;
+    private double longitude;
+    private double elevation;
+    private double deviceID;
+
+    public double getO2val() {
+        return o2val;
+    }
+
+    public void setO2val(double o2val) {
+        this.o2val = o2val;
+    }
+
+    public double getCo2val() {
+        return co2val;
+    }
+
+    public void setCo2val(double co2val) {
+        this.co2val = co2val;
+    }
+
+    public double getVocval() {
+        return vocval;
+    }
+
+    public void setVocval(double vocval) {
+        this.vocval = vocval;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
+
+    public double getDeviceID() {
+        return deviceID;
+    }
+
+    public void setDeviceID(double deviceID) {
+        this.deviceID = deviceID;
+    }
+}
