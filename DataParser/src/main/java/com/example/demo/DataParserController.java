7e5a528e418df38462f6c85346b4bb332a729b36
Rokib <syerokib@bell.corp.bce.ca>
2018-11-03 14:18:29 -0400

    adfs

--- DataParser/src/main/java/com/example/demo/DataParserController.java
+++ DataParser/src/main/java/com/example/demo/DataParserController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
 
 @RestController
 public class DataParserController {
+
+    public ArrayList arrayList = new ArrayList();
 
     @RequestMapping(
             value = "/process",
@@ -15,6 +18,7 @@ public class DataParserController {
             consumes = "application/json")
 
     public String process(@RequestBody DeviceData deviceData) throws Exception {
+        arrayList.add(deviceData.toString());
         return deviceData.toString();
     }
 
@@ -23,6 +27,7 @@ public class DataParserController {
             method = RequestMethod.GET)
 
     public String process(String hello) {
-        return "HELLO WORLD!";
+//        return "HELLO WORLD!";
+        return arrayList.toString();
     }
 }
